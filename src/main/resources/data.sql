043f20b1afbaae2c0f208af1a7506f61237545e1
juno <wnsgh732@gmail.com>
2025-06-10 15:39:39 +0900

    feat: 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
-INSERT INTO member(email, password) values ('123', '123')
+INSERT INTO member(email, password) values ('123', '123');
+
+INSERT INTO reservation_time(start_at) values ('9:00');
+INSERT INTO reservation_time(start_at) values ('10:00');
+INSERT INTO reservation_time(start_at) values ('11:00');
+INSERT INTO reservation_time(start_at) values ('12:00');
+INSERT INTO reservation_time(start_at) values ('13:00');
+INSERT INTO reservation_time(start_at) values ('14:00');
+INSERT INTO reservation_time(start_at) values ('15:00');
+INSERT INTO reservation_time(start_at) values ('16:00');
+INSERT INTO reservation_time(start_at) values ('17:00');
